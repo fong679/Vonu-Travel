4d604075eeaa71afffa2a84b53b9c4b4bf8a03af
fong679 <obacaufong@gmail.com>
2026-02-25 02:17:46 +0000

    Add PWA support with manifest and icons

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
 
-export default nextConfig;
+export default config({
+  eslint: { ignoreDuringBuilds: true },
+})
